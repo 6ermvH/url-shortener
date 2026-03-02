fed2199f8c44e3d607f883276e1d6ce13c9a263a
German Feskov <g.feskov@yandex.ru>
2026-03-02 19:01:07 +0300

    feat: add config loading and structured logging to main

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+
+	"github.com/6ermvH/url-shortener/internal/config"
+)
 
 func main() {
-	log.Println("Url-Shortener service")
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	cfg, err := config.Load()
+	if err != nil {
+		return fmt.Errorf("load config: %w", err)
+	}
+
+	log.Printf("Url-Shortener service: addr=%s storage=%s", cfg.Addr, cfg.Storage)
+
+	return nil
 }
